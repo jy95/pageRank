088914774ed2bd52345038f97c4c666fb185a5ec
jy95 <jacques.yakoub@gmail.com>
2017-11-26 01:52:08 +0100

    Set up the vector for Power method
    Vector normalized and ready to be used

--- pakerank.py
+++ pakerank.py
@@ -8,18 +8,18 @@ def pageRankScore(A: np.matrix, alpha: float = 0.9):
     print("Starting the program : Matrix of shape %s with alpha %f " % (A.shape, alpha))
     print(adj_matrix)
     # Vector of the sum for each column
-    indegree = adj_matrix.sum(axis=0).getA1()
-    print("indegree of each Node")
-    print(indegree)
+    in_degree = adj_matrix.sum(axis=0)
+    print("indegree of each node")
+    print(in_degree)
     print("Computing the probability matrix")
 
     # help us to not call sum multiple time when we will modify the matrix
-    outdegree = adj_matrix.sum(axis=1).getA1()
+    out_degree = adj_matrix.sum(axis=1).getA1()
 
     probability_matrix = []
     counter = 0
     for line in adj_matrix:
-        row = line.getA1() / outdegree[counter]
+        row = line.getA1() / out_degree[counter]
         probability_matrix.append(row)
         counter += 1
 
@@ -30,15 +30,29 @@ def pageRankScore(A: np.matrix, alpha: float = 0.9):
     transition_probability_matrix = probability_matrix.transpose()
     print(transition_probability_matrix)
 
+    print("Init vector")
+    vector = in_degree.transpose()
+    # Now time to normalize this vector by the sum
+    vector = vector / vector.sum()
+    print(vector)
+
+    # TODO Power method 
+
 
 # Read the matrix from csv and transform it to numpy matrix
-matrix = []
-cr = csv.reader(open("adjacenceMatrix.csv", "r"))
-for i, val in enumerate(cr):
-    matrix.append(val)
+def main():
 
-adj_matrix_np = np.matrix(matrix)
-pageRankScore(A=adj_matrix_np)
+    matrix = []
+    cr = csv.reader(open("adjacenceMatrix.csv", "r"))
 
-# Call with a custom alpha
-# pageRankScore(A=adj_matrix_np,alpha=0.8)
+    for i, val in enumerate(cr):
+        matrix.append(val)
+
+    adj_matrix_np = np.matrix(matrix)
+    pageRankScore(A=adj_matrix_np)
+
+    # Call with a custom alpha
+    # pageRankScore(A=adj_matrix_np,alpha=0.8)
+
+
+if __name__ == "__main__": main()
